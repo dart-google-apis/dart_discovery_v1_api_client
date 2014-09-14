874f502698aa8877bf49520ca3e0ff4e76700849
Adam Singer <financeCoding@gmail.com>
2014-09-13 18:51:59 -0700

    Automated update

--- lib/discovery_v1_api_browser.dart
+++ lib/discovery_v1_api_browser.dart
@@ -6,6 +6,7 @@ import 'package:google_discovery_v1_api/src/browser_client.dart';
 import "package:google_discovery_v1_api/discovery_v1_api_client.dart";
 
 /** Lets you discover information about other Google APIs, such as what APIs are available, the resource and method details for each API. */
+@deprecated
 class Discovery extends Client with BrowserClient {
 
   final oauth.OAuth2 auth;
--- lib/discovery_v1_api_console.dart
+++ lib/discovery_v1_api_console.dart
@@ -7,6 +7,7 @@ import 'package:google_discovery_v1_api/src/console_client.dart';
 import "package:google_discovery_v1_api/discovery_v1_api_client.dart";
 
 /** Lets you discover information about other Google APIs, such as what APIs are available, the resource and method details for each API. */
+@deprecated
 class Discovery extends Client with ConsoleClient {
 
   final oauth2.OAuth2Console auth;
